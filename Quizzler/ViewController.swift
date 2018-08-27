a461e9969c23c18ab7c368e6274b6d705660e4a7
Rafael Henriques <portuguese.dev@gmail.com>
2018-08-27 20:27:06 +0100

    View Changed to a different interface

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -11,18 +11,23 @@ import UIKit
 class ViewController: UIViewController {
     
     //Instance Variables
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer: Bool = false
+    var questionNumber: Int = 0 // Keep the state
+    var score: Int = 0
     
     // UI Elements from the storyboard are linked up here
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
-    @IBOutlet var progressBar: UIView!
+    @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var progressLabel: UILabel!
     
     // This gets called when the UIViewController is loaded into memory when the app starts
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        progressBar.transform = progressBar.transform.scaledBy(x: 1, y: 2)
+        nextQuestion()
     }
 
     // Nothing to modify here. This gets called when the system is low in memory
@@ -30,29 +35,64 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-    // This action is called when either the true or false button is pressed
+    // Called when either the true or false button is pressed
     @IBAction func answerPressed(_ sender: AnyObject) {
+        if sender.tag == 1 {
+            pickedAnswer = true
+        } else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
+        questionNumber += 1
+        nextQuestion()
         
     }
     
-    // This method will update all the views on screen (progress bar, score label, etc)
+    // Update all the views on screen (progress bar, score label, etc)
     func updateUI() {
-      
-    }
-    
-    // This method will update the question text and cehck if the end was reached
-    func nextQuestion() {
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "\(questionNumber) / 13"
+        progressBar.progress = Float(questionNumber) / 13
         
     }
     
+    // Update the question text and cehck if the end was reached
+    func nextQuestion() {
+        if questionNumber < 13 {
+            questionLabel.text = allQuestions.list[questionNumber].questionText
+        } else {
+            let alert = UIAlertController(title: "Awesome", message: "You've finished all the questions, do you want to start over?", preferredStyle: .alert)
+            
+            let restartAction = UIAlertAction(title: "Restart", style: .default, handler: {(UIAlertAction) in
+                    self.startOver()
+            })
+            
+            alert.addAction(restartAction)
+            
+            present(alert, animated: true, completion: nil)
+        }
+        updateUI()
+    }
     
+    // Check if the user has got the right answer
     func checkAnswer() {
         
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        
+        if correctAnswer == pickedAnswer {
+            score += 1
+            print("You got it!")
+        } else {
+            print("Wrong!")
+        }
     }
     
-    
+    // Wipe the board clean, so that the user can retake the quiz
     func startOver() {
-       
+        questionNumber = 0
+        score = 0
+        progressBar.progress = 0
     }
     
 
